95994ae2c0c9ffad6a925eaa10791f35cc5512ba
KremerMichael <kremeremichael@gmail.com>
2021-12-03 10:49:07 -0800

    another fix

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     name='messari',
     version='0.0.1',
     packages=['messari',
-              'messari.defillama']
+              'messari.defillama'],
+    url='',
     long_description=long_description,
     long_description_content_type='text/markdown',
     package_data={'messari': ['mappings/messari_to_dl.json']},
